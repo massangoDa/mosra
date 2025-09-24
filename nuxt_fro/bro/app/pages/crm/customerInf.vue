08434cbc926ab7bfc4cf058f72db4789569576d0
massangoDa <massango0303@outlook.jp>
2025-09-24 21:34:45 +0900

    add: 顧客情報ページを追加

---
+++ nuxt_fro/bro/app/pages/crm/customerInf.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Modal from "~/components/Modal.vue";
+
+definePageMeta({
+  layout: 'crm-layout',
+})
+
+const show = ref(false);
+</script>
+
+<template>
+<div>
+  <h1>顧客情報</h1>
+  <button @click="show = true">+ 顧客情報追加</button>
+  <Modal @closeModal="show = false" v-if="show" />
+</div>
+</template>
+
+<style scoped>
+
+</style>
