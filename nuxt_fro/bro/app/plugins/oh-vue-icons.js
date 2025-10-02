bb7ef5531df8a40c31c09fcab6ccc42a1c2a0481
massangoDa <massango0303@outlook.jp>
2025-10-02 21:51:34 +0900

    feat: ゴミ箱アイコンの追加

--- nuxt_fro/bro/app/plugins/oh-vue-icons.js
+++ nuxt_fro/bro/app/plugins/oh-vue-icons.js
@@ -16,9 +16,10 @@ import {
     MdHandshake,
     MdLogout,
     LaEditSolid,
+    LaTrashSolid,
 } from 'oh-vue-icons/icons';
 
-addIcons(FaSpotify, FaGithub, FaPencilAlt, HiHome, HiPencilAlt, BiInfoCircle, BiSearch, BiCalendarEvent, BiPcDisplay, MdDashboard, MdMessage, MdAnalytics, MdInfo, MdHandshake, MdLogout, LaEditSolid);
+addIcons(FaSpotify, FaGithub, FaPencilAlt, HiHome, HiPencilAlt, BiInfoCircle, BiSearch, BiCalendarEvent, BiPcDisplay, MdDashboard, MdMessage, MdAnalytics, MdInfo, MdHandshake, MdLogout, LaEditSolid, LaTrashSolid);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('v-icon', OhVueIcon);
