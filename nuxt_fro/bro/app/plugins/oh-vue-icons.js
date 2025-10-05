e76ddbecdcf5fbc96c97e0536367d175ace2f50b
massangoDa <massango0303@outlook.jp>
2025-10-05 21:47:29 +0900

    feat: pluginを編集。fullCalendarプラグインを追加

--- nuxt_fro/bro/app/plugins/oh-vue-icons.js
+++ nuxt_fro/bro/app/plugins/oh-vue-icons.js
@@ -17,9 +17,10 @@ import {
     MdLogout,
     LaEditSolid,
     LaTrashSolid,
+    IoWarning,
 } from 'oh-vue-icons/icons';
 
-addIcons(FaSpotify, FaGithub, FaPencilAlt, HiHome, HiPencilAlt, BiInfoCircle, BiSearch, BiCalendarEvent, BiPcDisplay, MdDashboard, MdMessage, MdAnalytics, MdInfo, MdHandshake, MdLogout, LaEditSolid, LaTrashSolid);
+addIcons(FaSpotify, FaGithub, FaPencilAlt, HiHome, HiPencilAlt, BiInfoCircle, BiSearch, BiCalendarEvent, BiPcDisplay, MdDashboard, MdMessage, MdAnalytics, MdInfo, MdHandshake, MdLogout, LaEditSolid, LaTrashSolid, IoWarning);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('v-icon', OhVueIcon);
