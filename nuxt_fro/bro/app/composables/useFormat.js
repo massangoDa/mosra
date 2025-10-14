68d9f7bd6a6f85c85ea2fccbe41391c06e49f5af
massangoDa <massango0303@outlook.jp>
2025-10-14 21:48:52 +0900

    feat: YYYY年MM月DD日に変換する機能を作成。

--- nuxt_fro/bro/app/composables/useFormat.js
+++ nuxt_fro/bro/app/composables/useFormat.js
@@ -9,8 +9,18 @@ export const useFormat = () => {
         return Number(value).toLocaleString() + currency
     }
 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}年${month}月${day}日`;
+    };
+
+
     return {
         addComma,
-        formatCurrency
+        formatCurrency,
+        formatDate,
     }
 }
