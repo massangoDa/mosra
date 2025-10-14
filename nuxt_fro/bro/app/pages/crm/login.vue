d186bd3c0c42e8edb4daac5dc75f5677dec98c7f
massangoDa <massango0303@outlook.jp>
2025-10-14 21:47:55 +0900

    update: ログイン画面を更新。ちょっぴり目がおかしくなる仕掛けを追加。

--- nuxt_fro/bro/app/pages/crm/login.vue
+++ nuxt_fro/bro/app/pages/crm/login.vue
@@ -34,22 +34,25 @@ const handleSubmit = async () => {
 
 <template>
   <div class="inf-container">
-    <div class="inf-left">
-      <h1>Login</h1>
-    </div>
-    <div class="inf-right">
-      <h2>Sign In to MassCRM</h2>
-      <form @submit.prevent="handleSubmit">
-        <div class="form-group">
-          <label for="email">Email Address</label>
-          <input type="email" id="email" v-model="email" placeholder="youremail@gmail.com" required :disabled="isLoading" />
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" id="password" v-model="password" placeholder="••••••••" required :disabled="isLoading" />
-        </div>
-        <button type="submit" class="sign-in-btn" :disabled="isLoading">{{ isLoading ? "サインイン中..." : "Sign In" }}</button>
-      </form>
+    <div class="inf">
+      <div class="inf-left">
+        <h1>Login</h1>
+      </div>
+      <div class="inf-right">
+        <h2>Sign In to MassCRM</h2>
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="email" id="email" v-model="email" placeholder="youremail@gmail.com" required :disabled="isLoading" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" v-model="password" placeholder="••••••••" required :disabled="isLoading" />
+          </div>
+          <button type="submit" class="sign-in-btn" :disabled="isLoading">{{ isLoading ? "サインイン中..." : "Sign In" }}</button>
+        </form>
+        <p>Don't have an account? <NuxtLink to="/crm/register">Signup Here</NuxtLink> </p>
+      </div>
     </div>
   </div>
 </template>
@@ -57,19 +60,36 @@ const handleSubmit = async () => {
 <style scoped>
 .inf-container {
   display: flex;
+  justify-content: center;
   height: 100vh;
   width: 100%;
+  background: linear-gradient(
+      to right,
+      #3f8cff calc(70px + (100vw - 167px) * 0.6),
+      #e6f0ff calc(70px + (100vw - 167px) * 0.6)
+  );
+  align-items: center;
+}
+
+.inf {
+  display: flex;
+  width: 100%;
+  height: 75%;
+  margin: 70px;
+  border-radius: 12px;
 }
 
 .inf-left {
   flex: 1.2;
-  background-color: #3f8cff;
   color: white;
   padding: 40px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  border-radius: 12px 0 0 12px;
+  box-shadow: 4px 4px 30px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
 }
 
 .inf-left h1 {
@@ -94,6 +114,12 @@ const handleSubmit = async () => {
   flex-direction: column;
   justify-content: center;
   background-color: #fff;
+  border-radius: 0 12px 12px 0;
+  box-sizing: border-box;
+}
+
+.inf-right p {
+  text-align: center;
 }
 
 .inf-right h2 {
