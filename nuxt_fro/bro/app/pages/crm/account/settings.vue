0b54da51966f1cb77097e4cdf9c7e7ac2044688e
massangoDa <massango0303@outlook.jp>
2025-10-16 21:53:24 +0900

    feat: アカウントセッティング画面を追加。

---
+++ nuxt_fro/bro/app/pages/crm/account/settings.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'crm-layout',
+})
+
+
+</script>
+
+<template>
+<div>
+  <div class="container">
+    <div class="header">
+      <div class="header-left">
+        <h1>アカウント設定</h1>
+      </div>
+      <div class="settings-container">
+        
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
