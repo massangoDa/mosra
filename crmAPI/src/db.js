022dd0bebc3c94a09fbc6d5a602207aabd3567e2
massangoDa <massango0303@outlook.jp>
2025-10-16 21:51:49 +0900

    feat: db接続機能を作成。Mongodb接続のためのurlが元々ハードコーディングだったので、.envで管理。

---
+++ crmAPI/src/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const url = process.env.MONGODB_URL;
+
+let db;
+let client;
+
+export async function connectToMongo() {
+    try {
+        client = await MongoClient.connect(url);
+        console.log("Mongoサーバーと接続されました")
+        db = client.db('crm');
+        return db;
+    } catch (err) {
+        console.log("Mongoとの接続でエラーが発生しました", err);
+        process.exit(1);
+    }
+}
+
+export { db };
+
+export async function closeConnection() {
+    if (client) {
+        await client.close();
+        console.log("データベースとの接続が閉じられました");
+    }
+}
