66c4c1361c7ed320d10d50fb3afa8b7c5b6f8dff
massangoDa <massango0303@outlook.jp>
2025-10-25 22:07:53 +0900

    feat: 設定画面の作成。汎用コンテナ使ってみた。

--- nuxt_fro/bro/app/pages/crm/account/settings.vue
+++ nuxt_fro/bro/app/pages/crm/account/settings.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import GridPageContainer from "~/components/PageContainer.vue";
+import '~/assets/css/pages/settings.css'
+import PageContainer from "~/components/PageContainer.vue";
+
 definePageMeta({
   layout: 'crm-layout',
 })
 
-
+const sidebarLink = [
+  {
+    name: 'アカウント',
+    icon: 'md-accountcircle',
+    to: '#'
+  }
+]
 </script>
 
 <template>
-<div>
-  <div class="container">
-    <div class="header">
-      <div class="header-left">
-        <h1>アカウント設定</h1>
-      </div>
-      <div class="settings-container">
-        
-      </div>
-    </div>
-  </div>
-</div>
+  <PageContainer title="設定" :sidebar="sidebarLink">
+    <p>名前</p>
+    <input type="text" />
+    <p>メールアドレス</p>
+    <input type="email">
+    <p>パスワード</p>
+    <input type="password" />
+  </PageContainer>
 </template>
 
 <style scoped>
