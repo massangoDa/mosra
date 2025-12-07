3886554b83ebd216696216397068102bcb82704d
massangoDa <massango0303@outlook.jp>
2025-12-07 20:00:25 +0900

    feat: 案件追加モーダルの設定を追加。

--- nuxt_fro/bro/app/pages/crm/customer/[customerId]/new/case.vue
+++ nuxt_fro/bro/app/pages/crm/customer/[customerId]/new/case.vue
@@ -41,13 +41,69 @@ onMounted(async () => {
   await loadCustomer()
 })
 
+const showCaseModal = ref(false)
+
+const submitCaseUrl = ``
+
+const caseFields = [
+  {
+    name: 'caseName',
+    label: '案件名',
+    type: 'text',
+  },
+  {
+    name: 'category',
+    label: '区分',
+    type: 'select',
+    options: ['保守', '単発', 'その他'],
+  },
+  {
+    name: 'caseStartDate',
+    label: '契約開始日',
+    type: 'date',
+    format: 'YYYY/MM/DD',
+  },
+  {
+    name: 'caseFinishDate',
+    label: '契約終了日',
+    type: 'date',
+    format: 'YYYY/MM/DD',
+  },
+  {
+    name: 'monthlyFee',
+    label: '金額',
+    type: 'number',
+  },
+  {
+    name: 'billingCycle',
+    label: '請求サイクル',
+    type: 'select',
+    options: ['毎月', '2ヶ月毎', '年1回', '一回のみ'],
+  }
+]
+
 </script>
 
 <template>
 <div>
   <PageContainer :title="customer?.companyName" :sidebar="sidebarLink">
+    <template #header-right>
+      <button @click="showCaseModal = true" class="NewInfoButton">+ 請求書追加</button>
+    </template>
     <h2>案件</h2>
   </PageContainer>
+  <TemplateModal
+      v-if="showCaseModal"
+      title="案件追加"
+      section-title="請求書を追加"
+      :submit-url="submitCaseUrl"
+      :fields="caseFields"
+      success-message="請求書を保存しました"
+      @close-modal="showCaseModal = false"
+      description="区分を『<span style='color:#d9534f; font-weight:bold;'>単発</span>』にしたときは、金額にそのまま請求額を入力し、請求サイクルには『<span style='color:#0275d8; font-weight:bold;'>1回のみ</span>』を選んでください。"
+
+      @refresh=""
+  />
 </div>
 </template>
 
