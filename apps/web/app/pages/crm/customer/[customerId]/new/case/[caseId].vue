a10b354fec3016a35e1ac769c22fc638030c988f
massangoda <mass47ngo@massango.jp>
2026-01-16 20:35:03 +0900

    feat(web): 顧客案件ページに、サイドバーリンクと顧客データの動的表示を追加

--- apps/web/app/pages/crm/customer/[customerId]/new/case/[caseId].vue
+++ apps/web/app/pages/crm/customer/[customerId]/new/case/[caseId].vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import {ref} from "vue";
 import { useToast } from 'vue-toastification'
 import {fetchCustomer} from "~/api/customer";
@@ -6,15 +6,51 @@ import {fetchTransactions} from "~/api/transactions";
 import {API_ENDPOINTS} from "~/api/endpoints";
 import {fetchInvoice} from "~/api/invoice";
 import '~/assets/css/pages/id.css'
+import type {Customer} from "~/types/types";
 
 definePageMeta({
-  layout: 'crm-layout',
+  layout: 'crm-layout'
+})
+
+const customer = ref<Customer | null>(null)
+
+const { customerId } = useRoute().params;
+
+const sidebarLink = [
+  {
+    name: '案件',
+    icon: 'md-lock',
+    to: `/crm/customer/${customerId}/new/cases`
+  },
+  {
+    name: 'サブスクリプション',
+    icon: 'md-lock',
+    to: `/crm/customer/${customerId}/new/subscription`
+  },
+  {
+    name: '詳細',
+    icon: 'md-lock',
+    to: `/crm/customer/${customerId}/new/details`
+  }
+]
+
+async function loadCustomer() {
+  try {
+    customer.value = await fetchCustomer(customerId)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onMounted(async () => {
+  await loadCustomer()
 })
 
 </script>
 
 <template>
   <div>
-    <h1>テスト</h1>
+    <PageContainer :title="customer?.companyName" :sidebar="sidebarLink">
+    </PageContainer>
   </div>
 </template>
