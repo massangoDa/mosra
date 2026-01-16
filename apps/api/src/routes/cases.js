bbd4156596ada88350fdc24a4b337e17795433eb
massangoda <mass47ngo@massango.jp>
2026-01-16 22:02:54 +0900

    feat(api): 案件詳細取得のAPIエンドポイントを追加

--- apps/api/src/routes/cases.js
+++ apps/api/src/routes/cases.js
@@ -5,5 +5,6 @@ import casesController from '../controllers/casesController.js';
 
 router.post('/', authenticateToken, casesController.createCase); // 単
 router.get('/', authenticateToken, casesController.getCases); // 複
+router.get('/:caseId', authenticateToken, casesController.getCase) // 単
 
 export default router;
