222c43f7c2adc4917865f9b08af782814a2dd939
massangoda <mass47ngo@massango.jp>
2026-01-22 19:37:49 +0900

    feat(web): 請求書ページとサイドバーリンクを追加

---
+++ apps/web/app/pages/crm/customer/[customerId]/new/case/[caseId]/invoices.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import '~/assets/css/pages/id.css'
+
+definePageMeta({
+  layout: 'crm-layout'
+})
+
+const { customerId, caseId } = useRoute().params;
+
+const sidebarLink = [
+  {
+    name: '案件一覧に戻る',
+    icon: 'md-keyboardreturn',
+    to: `/crm/customer/${customerId}/new/cases`
+  },
+  {
+    name: '案件内容',
+    icon: 'md-info',
+    to: `/crm/customer/${customerId}/new/case/${caseId}`
+  },
+  {
+    name: '請求書',
+    icon: 'md-eventrepeat',
+    to: `/crm/customer/${customerId}/new/case/${caseId}/invoices`
+  },
+]
+
+const showInvoiceModal = ref(false);
+</script>
+
+<template>
+  <div>
+    <PageContainer title="顧客管理" :sidebar="sidebarLink">
+      <template #header-right>
+        <button @click="showInvoiceModal = true" class="NewInfoButton">
+          + 請求書作成
+        </button>
+      </template>
+
+      <h2>請求書</h2>
+      <div class="section">
+        <div class="content">
+
+        </div>
+      </div>
+    </PageContainer>
+  </div>
+
+  <Modal
+      v-if="showInvoiceModal"
+      title="請求書追加"
+      section-title="請求情報を追加"
+    />
+</template>
+
+<style scoped>
+
+</style>
