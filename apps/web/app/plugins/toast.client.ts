5c3688622942e0e7cc77b49bdbb5f79833bc8074
toapuro <164835903+toapuro@users.noreply.github.com>
2026-01-31 08:15:18 +0900

    fix(web): toatプラグインのエラーの修正

--- apps/web/app/plugins/toast.client.ts
+++ apps/web/app/plugins/toast.client.ts
@@ -1,5 +1,6 @@
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
+import {defineNuxtPlugin} from "nuxt/app";
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(Toast, {
